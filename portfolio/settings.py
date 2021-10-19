127fa781f16d99abc05d3434eadad5dccde7fb9f
alissatroiano <alissatroianonyc@gmail.com>
2021-10-19 12:53:11 -0400

    Update base template to create block content tags and import assets

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -116,7 +116,6 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 STATIC_URL = '/static/'
@@ -124,6 +123,13 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+
+# Static and media files
+STATICFILES_LOCATION = 'static'
+
+# Media
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
