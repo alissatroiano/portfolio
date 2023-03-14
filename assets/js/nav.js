be84d7722c0b465f02d0bad32664d5d133115224
Alissa <alissatroianonyc@gmail.com>
2023-03-14 14:13:58 -0400

    Add fixed bottom navigation with styles

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -1,5 +1,5 @@
 const navLinks = document.querySelectorAll('.nav-item')
-const menuToggle = document.getElementById('navbarSupportedContent')
+const menuToggle = document.getElementById('navbarCollapse')
 const bsCollapse = new bootstrap.Collapse(menuToggle)
 navLinks.forEach((l) => {
     l.addEventListener('click', () => { bsCollapse.toggle() })
