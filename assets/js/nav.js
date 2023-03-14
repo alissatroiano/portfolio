43ec8d176b7bb71ff74ae4d535a0be8e1514bf3a
Alissa <alissatroianonyc@gmail.com>
2023-03-14 13:43:36 -0400

    Update navbar script to alleviate error on desktop

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -1,6 +1,6 @@
 const navLinks = document.querySelectorAll('.nav-item')
-const menuToggle = document.getElementById('navbarText')
-const bsCollapse = new bootstrap.Collapse(menuToggle, {toggle:false})
+const menuToggle = document.getElementById('navbarSupportedContent')
+const bsCollapse = new bootstrap.Collapse(menuToggle)
 navLinks.forEach((l) => {
     l.addEventListener('click', () => { bsCollapse.toggle() })
 })
