988f37d8d3b813dc5d7ca83e27d425962635b482
Alissa <alissatroianonyc@gmail.com>
2023-03-17 13:37:45 -0400

    Repair error that was causing navbar to automatically open on page load

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -1,6 +1,6 @@
 const navLinks = document.querySelectorAll('.nav-item')
 const menuToggle = document.getElementById('navbarCollapse')
-const bsCollapse = new bootstrap.Collapse(menuToggle)
+const bsCollapse = new bootstrap.Collapse(menuToggle, {toggle:false})
 navLinks.forEach((l) => {
     l.addEventListener('click', () => { bsCollapse.toggle() })
 })
