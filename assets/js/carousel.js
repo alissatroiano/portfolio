650ad75a0b48184525a2459260ddd48d83b8d5bc
Alissa <alissatroianonyc@gmail.com>
2023-06-05 22:30:13 -0400

    Create carousel for portfolio

--- assets/js/carousel.js
+++ assets/js/carousel.js
@@ -1,36 +1,42 @@
-$(function(){
-    $('.carousel-item').eq(0).addClass('active');
-    var total = $('.carousel-item').length;
-    var current = 0;
-    $('#moveRight').on('click', function(){
-      var next=current;
-      current= current+1;
-      setSlide(next, current);
-    });
-    $('#moveLeft').on('click', function(){
-      var prev=current;
-      current = current- 1;
-      setSlide(prev, current);
-    });
-    function setSlide(prev, next){
-      var slide= current;
-      if(next>total-1){
-       slide=0;
-        current=0;
-      }
-      if(next<0){
-        slide=total - 1;
-        current=total - 1;
-      }
-             $('.carousel-item').eq(prev).removeClass('active');
-             $('.carousel-item').eq(slide).addClass('active');
-        setTimeout(function(){
+document.addEventListener('DOMContentLoaded', function() {
+  var carouselItems = document.getElementsByClassName('carousel-item');
+  carouselItems[0].classList.add('active');
+  var total = carouselItems.length;
+  var current = 0;
   
-        },800);
-      
-  
-      
-      console.log('current '+current);
-      console.log('prev '+prev);
-    }
+  document.getElementById('moveRight').addEventListener('click', function() {
+    var next = current;
+    current = current + 1;
+    setSlide(next, current);
   });
+  
+  document.getElementById('moveLeft').addEventListener('click', function() {
+    var prev = current;
+    current = current - 1;
+    setSlide(prev, current);
+  });
+  
+  function setSlide(prev, next) {
+    var slide = current;
+    
+    if (next > total - 1) {
+      slide = 0;
+      current = 0;
+    }
+    
+    if (next < 0) {
+      slide = total - 1;
+      current = total - 1;
+    }
+    
+    carouselItems[prev].classList.remove('active');
+    carouselItems[slide].classList.add('active');
+    
+    setTimeout(function() {
+      // Code to be executed after 800 milliseconds
+    }, 800);
+    
+    console.log('current ' + current);
+    console.log('prev ' + prev);
+  }
+});
