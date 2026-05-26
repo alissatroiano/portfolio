44f31295162189c2d9cc695acf0d676706d975a6
Alissa Troiano <alissatroianonyc@gmail.com>
2026-05-26 18:47:13 -0400

    Update nav

--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -226,9 +226,6 @@ function buildModal(p) {
   modal.innerHTML = `
     <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable w-100">
       <div class="modal-content">
-
-        <div class="modal-header">
-          // <canvas class="modal-header-canvas"></canvas>
           <div class="modal-header-inner">
             <div class="modal-header-copy">
               <div class="modal-eyebrow">${p.categories.join(' · ')}</div>
@@ -239,7 +236,6 @@ function buildModal(p) {
             </div>
             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
           </div>
-        </div>
 
         <div class="modal-body">
           ${bodyHtmlFinal}
